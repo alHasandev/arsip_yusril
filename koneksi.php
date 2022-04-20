b07c73ff916a6f3a2438da70c3a3ae86f412700b
alHasandev <alhasan99dev@gmail.com>
2022-04-20 14:12:04 +0800

    build: setup fallback connection for deployment

--- koneksi.php
+++ koneksi.php
@@ -5,9 +5,9 @@ require_once "vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
-$host = $_ENV['HOSTNAME'] ?? "localhost";
-$username = $_ENV['USERNAME'] ?? "root";
-$password = $_ENV['PASSWORD'] ?? "";
-$dbname =  $_ENV['DATABASE_NAME'] ?? "pt_baktiputra";
+$host = $_ENV['HOSTNAME'] ?? "alhasand_arsip_yusril";
+$username = $_ENV['USERNAME'] ?? "alhasand_arsip_yusril";
+$password = $_ENV['PASSWORD'] ?? "arsip_yusril";
+$dbname =  $_ENV['DATABASE_NAME'] ?? "alhasand_arsip_yusril";
 
 $koneksi = new mysqli($host, $username, $password, $dbname);
