5b48003106fa0f538f04d6cc87089e5ae1c83320
alHasandev <alhasan99dev@gmail.com>
2022-04-20 12:00:47 +0800

    fix: removing var_dump

--- koneksi.php
+++ koneksi.php
@@ -2,10 +2,11 @@
 
 require_once "vendor/autoload.php";
 
-
 $host = "localhost";
-$username = "alhasand_arsip_yusril";
-$password = "arsip_yusril";
-$dbname = "alhasand_arsip_yusril";
+$username = "root";
+$password = "";
+$dbname =  "pt_baktiputra";
+
+var_dump([$host, $username, $password, $dbname]);
 
 $koneksi = new mysqli($host, $username, $password, $dbname);
