02d50060bf6d152b84d681795b43d620daa2ba58
alHasandev <alhasan99dev@gmail.com>
2022-04-20 11:46:13 +0800

    test: set connection configuration for deployment

--- koneksi.php
+++ koneksi.php
@@ -9,10 +9,10 @@ require_once "vendor/autoload.php";
 
 
 $host = @$_ENV['HOSTNAME'] ? $_ENV['HOSTNAME'] : "localhost";
-$username = @$_ENV['USERNAME'] ? $_ENV['USERNAME'] : "root";
-$password = @$_ENV['PASSWORD'] ? $_ENV['PASSWORD'] : "";
-$dbname = @$_ENV['DATABASE_NAME'] ? $_ENV['DATABASE_NAME'] : "pt_baktiputra";
+$username = @$_ENV['USERNAME'] ? $_ENV['USERNAME'] : "alhasand_arsip_yusril";
+$password = @$_ENV['PASSWORD'] ? $_ENV['PASSWORD'] : "arsip_yusril";
+$dbname = @$_ENV['DATABASE_NAME'] ? $_ENV['DATABASE_NAME'] : "alhasand_arsip_yusril";
 
-var_dump([$host, $username, $password, $dbname]);
+var_dump([$_ENV['HOSTNAME'], $_ENV['USERNAME'], $_ENV['PASSWORD'], $_ENV['DATABASE_NAME']]);
 
 $koneksi = new mysqli($host, $username, $password, $dbname);
