0e73ad38546248160865920a112c7ecaf1d65934
alHasandev <alhasan99dev@gmail.com>
2022-04-20 11:59:29 +0800

    fix: removing  usage

--- koneksi.php
+++ koneksi.php
@@ -8,6 +8,4 @@ $username = "alhasand_arsip_yusril";
 $password = "arsip_yusril";
 $dbname = "alhasand_arsip_yusril";
 
-var_dump([$_ENV['HOSTNAME'], $_ENV['USERNAME'], $_ENV['PASSWORD'], $_ENV['DATABASE_NAME']]);
-
 $koneksi = new mysqli($host, $username, $password, $dbname);
